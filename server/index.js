715603951ea80e4d7b8f700e333cefbae1d6c043
Nishan9t <pandeynisha9t@gmail.com>
2023-08-19 12:06:24 +0530

    server is running

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors=require('cors');
+
+
+
+const app=express();
+app.use(cors());
+
+
+app.listen(8000,()=>{
+    console.log("server is running on 8000");
+})
